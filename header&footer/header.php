215a83b9070318e81eaf486aef08b22de2d181eb
prathamraval33 <prathamraval3326@gmail.com>
2025-01-16 20:59:00 +0530

    FOOTER ADDED

---
+++ header&footer/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movify</title>
+    <link rel="stylesheet" href="../css/header.css">
+    
+   
+</head>
+<body>
+    <nav class="navbar">
+    
+        <div class="logo">
+            <a href="#"><img src="../img/popflix.png" class="logoimg"></a>
+            
+        </div>
+            <div class="menu">
+                
+                <a href="#"> HOME</a>
+                <a href="#" style="margin-left: 30px;"  > ABOUT US</a>
+                <a href="#"style="margin-left: 30px;"> CONTACT US</a>
+                <a href="#" style="margin-left: 30px;"> STORY </a>
+                  <div class="dropdown">
+                    <b><p>MOVIE</p></b>
+                    <div class="dropdown-content">
+                      <a href="#">Now Showing </a>
+                      <a href="#">Upcoming</a>
+                     
+                    </div>
+                   
+                 
+
+                </div>
+                </div>
+                <div class="sign">
+                    <img src="../img/profile.png" alt="profile" class="profile">
+                    <div class="signcontent">
+                    <a href="#" >sign in</a>
+                    <a href="#" >sign up</a>
+                    </div>
+                </div>
+               
+            
+            </nav>
+           
+            
+</body>
+</html>
